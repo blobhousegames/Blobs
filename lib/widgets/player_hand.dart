d2d092cd09d27cddae27739a0d09f3968c5c7067
Carl Nield <carlnield@CarlsMacBookNeo.modem>
2026-08-07 20:07:38 +1000

    Add animated card dealing to player hand

--- lib/widgets/player_hand.dart
+++ lib/widgets/player_hand.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 import '../models/playing_card.dart';
 import 'playing_card_widget.dart';
 
-class PlayerHand extends StatelessWidget {
+import 'dart:async';
+
+
+
+class PlayerHand extends StatefulWidget {
   const PlayerHand({
     required this.cards,
     required this.legalCards,
@@ -20,6 +24,65 @@ class PlayerHand extends StatelessWidget {
   final ValueChanged<PlayingCard> onCardSelected;
 
   @override
+  State<PlayerHand> createState() => _PlayerHandState();
+}
+
+class _PlayerHandState extends State<PlayerHand> {
+  Timer? _dealTimer;
+  int _visibleCardCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _startDealAnimation();
+  }
+
+  @override
+  void didUpdateWidget(covariant PlayerHand oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.cards.length != widget.cards.length &&
+        widget.cards.length > oldWidget.cards.length) {
+      _startDealAnimation();
+    }
+  }
+
+  @override
+  void dispose() {
+    _dealTimer?.cancel();
+    super.dispose();
+  }
+
+  void _startDealAnimation() {
+    _dealTimer?.cancel();
+
+    _visibleCardCount = 0;
+
+    if (widget.cards.isEmpty) {
+      return;
+    }
+
+    _dealTimer = Timer.periodic(
+      const Duration(milliseconds: 120),
+      (timer) {
+        if (!mounted) {
+          timer.cancel();
+          return;
+        }
+
+        if (_visibleCardCount >= widget.cards.length) {
+          timer.cancel();
+          return;
+        }
+
+        setState(() {
+          _visibleCardCount++;
+        });
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 122,
@@ -28,23 +91,46 @@ class PlayerHand extends StatelessWidget {
           horizontal: 18,
         ),
         scrollDirection: Axis.horizontal,
-        itemCount: cards.length,
+        itemCount: widget.cards.length,
         separatorBuilder: (_, _) =>
             const SizedBox(width: 8),
         itemBuilder: (context, index) {
-          final card = cards[index];
+          final card = widget.cards[index];
 
           final isLegal =
-              enabled && legalCards.contains(card);
+              widget.enabled &&
+              widget.legalCards.contains(card);
 
-          return GestureDetector(
-            onTap: isLegal
-                ? () => onCardSelected(card)
-                : null,
-            child: PlayingCardWidget(
-              card: card,
-              isPlayable: isLegal,
-              isSelected: selectedCard == card,
+          final isVisible =
+              index < _visibleCardCount;
+
+          return AnimatedOpacity(
+            duration: const Duration(
+              milliseconds: 180,
+            ),
+            opacity: isVisible ? 1 : 0,
+            child: AnimatedSlide(
+              duration: const Duration(
+                milliseconds: 220,
+              ),
+              curve: Curves.easeOutCubic,
+              offset: isVisible
+                  ? Offset.zero
+                  : const Offset(0, 0.7),
+              child: IgnorePointer(
+                ignoring: !isVisible,
+                child: GestureDetector(
+                  onTap: isLegal
+                      ? () => widget.onCardSelected(card)
+                      : null,
+                  child: PlayingCardWidget(
+                    card: card,
+                    isPlayable: isLegal,
+                    isSelected:
+                        widget.selectedCard == card,
+                  ),
+                ),
+              ),
             ),
           );
         },
